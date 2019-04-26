eb9a83c7152713a8a4f3dee4de11775ded617c9d
Shane Huston <shuston@pivotal.io>
2019-04-26 12:40:58 -0400

    Add integration test for caching
    [#165546217]
    
    Co-authored-by: Daniel Thornton <dthornton@pivotal.io>

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -1,7 +1,12 @@
 package integration
 
 import (
+	"bytes"
+	"io"
+	"os"
+	"os/exec"
 	"path/filepath"
+	"regexp"
 	"testing"
 
 	"github.com/sclevine/spec"
@@ -42,5 +47,45 @@ func testIntegration(t *testing.T, when spec.G, it spec.S) {
 
 		_, _, err = app.HTTPGet("/")
 		Expect(err).NotTo(HaveOccurred())
+	})
+
+	when("the app is pushed twice", func() {
+		it("does not reinstall go modules", func() {
+			appDir := filepath.Join("testdata", "simple_app")
+			app, err := dagger.PackBuild(appDir, goBuildpack, packageBuildpack)
+			Expect(err).ToNot(HaveOccurred())
+			defer app.Destroy()
+
+			Expect(app.BuildLogs()).To(MatchRegexp("go: finding github.com/"))
+			Expect(app.BuildLogs()).To(MatchRegexp("go: downloading github.com/"))
+			Expect(app.BuildLogs()).To(MatchRegexp("go: extracting github.com/"))
+
+			buildLogs := &bytes.Buffer{}
+
+			// TODO: Move this to dagger
+
+			_, imageID, _, err := app.Info()
+			Expect(err).NotTo(HaveOccurred())
+
+			cmd := exec.Command("pack", "build", imageID, "--builder", "cfbuildpacks/cflinuxfs3-cnb-test-builder", "--buildpack", goBuildpack, "--buildpack", packageBuildpack)
+			cmd.Dir = appDir
+			cmd.Stdout = io.MultiWriter(os.Stdout, buildLogs)
+			cmd.Stderr = io.MultiWriter(os.Stderr, buildLogs)
+			Expect(cmd.Run()).To(Succeed())
+
+			const ansi = "[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))"
+
+			re := regexp.MustCompile(ansi)
+			strippedLogs := re.ReplaceAllString(buildLogs.String(), "")
+
+			Expect(strippedLogs).NotTo(MatchRegexp("go: finding github.com/"))
+			Expect(strippedLogs).NotTo(MatchRegexp("go: downloading github.com/"))
+			Expect(strippedLogs).NotTo(MatchRegexp("go: extracting github.com/"))
+
+			Expect(app.Start()).To(Succeed())
+
+			_, _, err = app.HTTPGet("/")
+			Expect(err).NotTo(HaveOccurred())
+		})
 	})
 }
