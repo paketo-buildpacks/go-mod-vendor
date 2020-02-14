e60fa0b42d342d61e3619e785b16f894604cdf99
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2020-02-14 14:14:39 +0000

    Update go module github.com/cloudfoundry/dagger

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.12
 
 require (
 	github.com/buildpack/libbuildpack v1.25.11
-	github.com/cloudfoundry/dagger v0.0.0-20200114192520-43fb0b3ec98f
+	github.com/cloudfoundry/dagger v0.0.0-20200213200846-c2a9723f08c4
 	github.com/cloudfoundry/libcfbuildpack v1.91.23
 	github.com/golang/mock v1.3.1
 	github.com/onsi/gomega v1.8.1
--- vendor/github.com/cloudfoundry/dagger/pack.go
+++ vendor/github.com/cloudfoundry/dagger/pack.go
@@ -8,9 +8,9 @@ import (
 	"io"
 	"os"
 	"sort"
+	"strings"
 	"sync"
 
-	"code.cloudfoundry.org/lager"
 	"github.com/buildpack/libbuildpack/logger"
 	"github.com/cloudfoundry/dagger/utils"
 	"github.com/cloudfoundry/packit/pexec"
@@ -40,7 +40,7 @@ var (
 )
 
 type Executable interface {
-	Execute(pexec.Execution) (stdout, stderr string, err error)
+	Execute(pexec.Execution) error
 }
 
 type Pack struct {
@@ -169,7 +169,7 @@ func NewPack(dir string, options ...PackOption) Pack {
 
 	pack := Pack{
 		dir:        dir,
-		executable: pexec.NewExecutable("pack", lager.NewLogger("pack")),
+		executable: pexec.NewExecutable("pack"),
 	}
 
 	for _, option := range options {
@@ -206,12 +206,14 @@ func (p Pack) Build() (*App, error) {
 	}
 
 	if p.offline {
-		// probably want to pull here?
-		dockerLogger := lager.NewLogger("docker")
-		dockerExec := pexec.NewExecutable("docker", dockerLogger)
+		dockerExec := pexec.NewExecutable("docker")
 
-		stdout, stderr, err := dockerExec.Execute(pexec.Execution{
-			Args: []string{"pull", builderImage},
+		stdout := bytes.NewBuffer(nil)
+		stderr := bytes.NewBuffer(nil)
+		err := dockerExec.Execute(pexec.Execution{
+			Args:   []string{"pull", builderImage},
+			Stdout: stdout,
+			Stderr: stderr,
 		})
 		if err != nil {
 			return nil, fmt.Errorf("failed to pull %s\n with stdout %s\n stderr %s\n%s", builderImage, stdout, stderr, err.Error())
@@ -224,7 +226,7 @@ func (p Pack) Build() (*App, error) {
 	}
 
 	buildLogs := bytes.NewBuffer(nil)
-	_, _, err = p.executable.Execute(pexec.Execution{
+	err = p.executable.Execute(pexec.Execution{
 		Args:   packArgs,
 		Stdout: buildLogs,
 		Stderr: buildLogs,
@@ -232,11 +234,12 @@ func (p Pack) Build() (*App, error) {
 	})
 
 	if err != nil {
-		printErr := printBufferSafely(buildLogs, os.Stdout)
+		output := &strings.Builder{}
+		printErr := printBufferSafely(buildLogs, output)
 		if printErr != nil {
 			return nil, printErr
 		}
-		return nil, err
+		return nil, fmt.Errorf("failed to pack build with output:\n%s\n--> error message: %w", output, err)
 	}
 
 	sum := sha256.Sum256([]byte(fmt.Sprintf("index.docker.io/library/%s:latest", p.image))) //This is how pack makes cache image names
