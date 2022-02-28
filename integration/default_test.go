136919d6f232dadc3a4d4dfc5e09794ec6665d5a
Victoria Henry <vhenry@vmware.com>
2022-02-28 16:38:11 -0500

    SBOM via packit v2

--- integration/default_test.go
+++ integration/default_test.go
@@ -29,8 +29,10 @@ func testDefault(t *testing.T, context spec.G, it spec.S) {
 
 	context("when building a simple go mod app", func() {
 		var (
-			image     occam.Image
-			container occam.Container
+			image      occam.Image
+			container1 occam.Container
+			container2 occam.Container
+			container3 occam.Container
 
 			name   string
 			source string
@@ -43,7 +45,9 @@ func testDefault(t *testing.T, context spec.G, it spec.S) {
 		})
 
 		it.After(func() {
-			Expect(docker.Container.Remove.Execute(container.ID)).To(Succeed())
+			Expect(docker.Container.Remove.Execute(container1.ID)).To(Succeed())
+			Expect(docker.Container.Remove.Execute(container2.ID)).To(Succeed())
+			Expect(docker.Container.Remove.Execute(container3.ID)).To(Succeed())
 			Expect(docker.Volume.Remove.Execute(occam.CacheVolumeNames(name))).To(Succeed())
 			Expect(docker.Image.Remove.Execute(image.ID)).To(Succeed())
 			Expect(os.RemoveAll(source)).To(Succeed())
@@ -74,20 +78,51 @@ func testDefault(t *testing.T, context spec.G, it spec.S) {
 				"    Running 'go mod vendor'",
 				MatchRegexp(`      Completed in ([0-9]*(\.[0-9]*)?[a-z]+)+`),
 				"",
+				"  Generating SBOM",
+				MatchRegexp(`      Completed in ([0-9]*(\.[0-9]*)?[a-z]+)+`),
 			))
 
-			container, err = docker.Container.Run.
+			container1, err = docker.Container.Run.
 				WithCommand("ls -alR /workspace").
 				Execute(image.ID)
 			Expect(err).NotTo(HaveOccurred())
 
 			Eventually(func() fmt.Stringer {
-				logs, _ = docker.Container.Logs.Execute(container.ID)
+				logs, _ = docker.Container.Logs.Execute(container1.ID)
 				return logs
 			}).Should(SatisfyAll(
 				ContainSubstring("go.sum"),
 				ContainSubstring("vendor/github.com/BurntSushi"),
 				ContainSubstring("vendor/github.com/satori"),
+			))
+
+			sbomFile := "/layers/sbom/launch/paketo-buildpacks_go-mod-vendor/mod-cache/sbom.cdx.json"
+			container2, err = docker.Container.Run.
+				WithCommand(fmt.Sprintf("cat %s", sbomFile)).
+				Execute(image.ID)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func() fmt.Stringer {
+				logs, _ = docker.Container.Logs.Execute(container2.ID)
+				return logs
+			}).Should(SatisfyAll(
+				ContainSubstring(`"bomFormat": "CycloneDX"`),
+			))
+
+			// check that all expected SBOM files are present
+			sbomDir := "/layers/sbom/launch/paketo-buildpacks_go-mod-vendor/mod-cache/"
+			container3, err = docker.Container.Run.
+				WithCommand(fmt.Sprintf("ls -al %s", sbomDir)).
+				Execute(image.ID)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func() fmt.Stringer {
+				logs, _ = docker.Container.Logs.Execute(container3.ID)
+				return logs
+			}).Should(And(
+				ContainSubstring("sbom.cdx.json"),
+				ContainSubstring("sbom.spdx.json"),
+				ContainSubstring("sbom.syft.json"),
 			))
 		})
 	})
